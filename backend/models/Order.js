d3d051c5ff0dd7d7f30399ac592baf05e9454e72
Nigam Subedi <nigam333nigam@gmail.com>
2026-04-01 20:22:09 +0545

    modifying register controller

--- backend/models/Order.js
+++ backend/models/Order.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt"
 
 const orderSchema = mongoose.Schema({
     name: {
@@ -19,6 +20,25 @@ const orderSchema = mongoose.Schema({
         required : true,
     },
 
+})
+
+orderSchema.pre("save", async function(next){
+    //only hash the password if it is modified
+    // 'this' refers to the document being saved on controller
+    if(!this.isModified("password")){
+        return next()
+    }
+    try{
+        // Generate salt 
+        const salt = await bcrypt.genSalt(10)
+
+        // Hash Password
+        this.password = await bcrypt.hash(this.password, salt);
+
+        next();
+    }catch(error){
+        next(error)
+    }
 })
 
 export const Order = mongoose.model("Order", orderSchema);
